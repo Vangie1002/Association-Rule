0b9977a74d12812f78b743ea84477ab2bc8edf2f
vangie1002 <vangie1002@gmail.com>
2020-02-19 14:40:09 +0100

    commit by R

--- BM.R
+++ BM.R
@@ -29,7 +29,8 @@ write.csv(data,file="data.csv",row.names = FALSE)
 #creating transition matrix
 
 tran <- read.transactions('data.csv', format = c('single'), sep=',', cols = c("order", "sku"), header = TRUE)
-
+tran
+summary(tran)
 inspect(tran)
 # number of transactions
 length(tran)
@@ -42,15 +43,15 @@ itemLabels(tran)
 # Visualize
 image(sample(tran))
 
-itemFrequencyPlot(tran,topN=20,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
+itemFrequencyPlot(tran,topN=10,type="absolute",col=brewer.pal(8,'Pastel2'), main="Absolute Item Frequency Plot")
 
 
 # Calculate rules use Apriori algorithm and specifying support and confidency 
 rules=apriori(tran,parameter=list(support=0.00005,confidence=0.00006,maxlen=3))
 
 length(rules) # 5005 rules
-subset.Rules<-rules[quality(rules)$confidence>0.4]
-length(subset.Rules)  # 122
+subset.Rules<-rules[quality(rules)$confidence>0.6]
+length(subset.Rules)  # 53
 
 # Inspect the top 10 rules in term of confidence 
 inspect(rules)
@@ -58,13 +59,15 @@ inspect(head(sort(rules,by='lift'),10))
 inspect(subset.Rules)
 inspect(head(sort(subset.Rules,by='lift'),10))
 top10subRules <- head(subset.Rules, n = 10, by = "confidence")
+inspect(top10subRules)
 # Visulize the rules 
 plot(rules,jitter=0)
 plot(subset.Rules,method="grouped matrix")
 plot(top10subRules, method = "graph",  engine = "htmlwidget")
 plot(subset.Rules,method="matrix")
 plot(head(sort(rules,by='lift'),20),method="graph")
-plot(head(sort(subset.Rules,by='lift'),20),method="graph")
+plot(head(sort(top10subRules,by='confidence'),10),method="graph")
+plot(head(sort(subset.Rules,by='lift'),5),method="grouped matrix")
 
 saveAsGraph(head(subset.Rules, n = 100, by = "lift"), file = "rules.graphml")
 # Filter top 20 rules with highest lift
